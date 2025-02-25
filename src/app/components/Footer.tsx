de3ddc8a35377273784335e9edbf5bbb9822173b
SanketPathare <sanketpathare8808@gmail.com>
2025-02-25 18:46:01 +0530

    10-commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,22 +3,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 py-4 px-6  border-gray-800">
+    <footer className="bg-gray-900 py-3 sm:py-4 px-3 sm:px-6 border-gray-800">
       <div className="max-w-6xl mx-auto">
-        <div className="mt-12 pt-6 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center">
-          <p className="text-gray-400 text-sm">
+        <div className="mt-8 sm:mt-12 pt-4 sm:pt-6 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
+          <p className="text-gray-400 text-xs sm:text-sm">
             &copy; {new Date().getFullYear()} AI Cheat Sheet Generator. All
             rights reserved.
           </p>
-          <div className="mt-4 md:mt-0 flex space-x-4">
+          <div className="mt-3 md:mt-0 flex space-x-4">
             <Link
-              href="
-          https://www.github.com/sanketpathare"
+              href="https://www.github.com/sanketpathare"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 text-sm"
+              className="text-gray-400 text-xs sm:text-sm"
             >
-              Build by:{" "}
+              Built by:{" "}
               <span className="underline hover:text-gray-300">
                 Sanket Pathare
               </span>
